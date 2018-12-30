3e09576f4461e226cfbb4c37ce092d43d71206e5
陈勇江 <chenyj796@126.com>
2018-12-30 11:54:56 +0800

    更新PatchVersion

--- Tool/PatchVersion/Program.cs
+++ Tool/PatchVersion/Program.cs
@@ -16,7 +16,7 @@ namespace PatchVersion
     /// 此程序用来在编译后，修改AssemblyInformationVersionAttribute及文件属性中的产品版本信息
     /// 使用方法：
     /// 在项目属性的“生成事件”页中的“后期生成事件命令行”中输入如下的命令：
-    /// PatchVersion.exe $(OutputPath)<Project>.dll
+    /// PatchVersion.exe $(OutputPath)$(AssemblyName).dll
     /// 注：假定PatchVersion.exe工具已经放到项目文件夹下
     ///     $(OutputPath)变量表示目标生成输出文件夹
     /// </summary>
@@ -26,23 +26,25 @@ namespace PatchVersion
         {
             if (args.Length == 0)
             {
-                throw new Exception($"请指定待打包的程序集文件");
+                Console.WriteLine($"{nameof(PatchVersion)}: 请指定待打包的程序集文件");
+                return;
             }
+
             var file = Path.GetFullPath(args[0]);
             try
             {
 
-                switch (Path.GetExtension(file).ToLowerInvariant())
+                switch (Path.GetExtension(file).ToLower())
                 {
                     case ".dll":
                     case ".exe":
                         if (!File.Exists(file))
                         {
-                            throw new Exception($"程序集文件“{file}”不存在");
+                            throw new Exception("文件不存在");
                         }
                         break;
                     default:
-                        throw new Exception($"程序集文件“{file}”的扩展名无效");
+                        throw new Exception("扩展名无效");
                 }
 
                 var information = BuildInformation(file);
@@ -51,13 +53,14 @@ namespace PatchVersion
                     UpdateWindowsResources(stream, information);
                     UpdateAssemblyAttributes(stream, information);
                 }
-                Console.WriteLine($@"
-给程序集文件“{file}”打包版本信息成功，写入如下的信息：
-    {information}");
+                var message = new StringBuilder()
+                    .AppendLine($"{nameof(PatchVersion)}: 给文件“{file}”打包版本信息成功，写入如下的内容：")
+                    .Append($"\t{information}");
+                Console.WriteLine(message.ToString());
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"给程序集文件“{file}”打包版本信息失败：{ex.Message}");
+                Console.WriteLine($"{nameof(PatchVersion)}: 给文件“{file}”打包版本信息失败：{ex.Message}");
             }
         }
 
@@ -91,21 +94,21 @@ namespace PatchVersion
 
                 var time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
                 var name = Exec("git", "symbolic-ref --short -q HEAD", path);
-                var commit = Exec("git", "rev-parse HEAD", path);
-                var addr = Exec("git", "remote get-url --all origin", path);
-                if (string.IsNullOrEmpty(addr))
+                var hash = Exec("git", "rev-parse HEAD", path);
+                var repo = Exec("git", "remote get-url --all origin", path);
+                if (string.IsNullOrEmpty(repo))
                 {
-                    addr = path;
+                    repo = path;
                 }
-                return $"编译({time}) 分支({name}) 提交({commit}) 地址({addr})";
+                return $"编译({time}) 分支({name}) 提交({hash}) 仓库({repo})";
             }
 
-            throw new Exception($"文件“{file}”未找到对应的git目录");
+            throw new Exception($"未找到对应的git目录");
         }
 
         /// <summary>
         /// 更新Windows文件中的资源，使得可以在文件“属性”对话框“详细信息”页的看到“产品版本”
-        /// 注：此方法参考自https://github.com/304NotModified/Fody.Stamp
+        /// 注：此方法源自https://github.com/304NotModified/Fody.Stamp
         /// </summary>
         /// <param name="stream"></param>
         /// <param name="information"></param>
@@ -121,11 +124,12 @@ namespace PatchVersion
             var reader = new VersionResourceReader(versionStream);
             var versions = reader.Read();
 
+            const string entry = "ProductVersion";
             foreach (var table in versions.StringFileInfo)
             {
-                if (table.Values.ContainsKey("ProductVersion"))
+                if (table.Values.ContainsKey(entry))
                 {
-                    table.Values["ProductVersion"] = information;
+                    table.Values[entry] = information;
                 }
             }
 
@@ -150,9 +154,10 @@ namespace PatchVersion
                 argument = new CAArgument(argument.Type, information);
                 attribute.ConstructorArguments.RemoveAt(0);
                 attribute.ConstructorArguments.Add(argument);
+
+                stream.Seek(0L, SeekOrigin.Begin);
+                module.Write(stream);
             }
-            stream.Seek(0L, SeekOrigin.Begin);
-            module.Write(stream);
         }
 
     }
