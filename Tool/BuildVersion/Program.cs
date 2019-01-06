05ab9ee75b8123d92823873a4a62b267f29a10a5
陈勇江 <chenyj796@126.com>
2019-01-06 15:49:27 +0800

    修改BuildVersion

--- Tool/BuildVersion/Program.cs
+++ Tool/BuildVersion/Program.cs
@@ -5,8 +5,10 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace BuildVersion
 {
@@ -25,16 +27,21 @@ namespace BuildVersion
                 {
                     throw new Exception("请指定项目文件夹");
                 }
-                var path = args[0];
-                if (!Directory.Exists(path))
+                var projectPath = args[0];
+                if (!Directory.Exists(projectPath))
                 {
-                    throw new Exception($"指定的项目文件夹“{path}”不存在");
+                    throw new Exception($"指定的项目文件夹“{projectPath}”不存在");
                 }
-                var directory = Path.Combine(path, "Properties");
-                Directory.CreateDirectory(directory);
-                var file = Path.Combine(directory, OutputFile);
+                var propertiesDir = Path.Combine(projectPath, "Properties");
+                Directory.CreateDirectory(propertiesDir);
+                var informationVersionFile = Path.Combine(propertiesDir, OutputFile);
+                if (!File.Exists(informationVersionFile)) //以前没有执行过
+                {
+                    AmendProjectFile(projectPath);
+                    AmendPropertyInfo(propertiesDir);
+                }
 
-                var information = BuildInformation(path);
+                var information = BuildInformation(projectPath);
                 information = $"[assembly: System.Reflection.AssemblyInformationalVersion(\"{information}\")]";
 
                 var assembly = Assembly.GetExecutingAssembly();
@@ -43,11 +50,11 @@ namespace BuildVersion
                 using (var reader = new StreamReader(stream))
                 {
                     var readme = reader.ReadToEnd();
-                    File.WriteAllLines(file, new[] { readme, information });
+                    File.WriteAllLines(informationVersionFile, new[] { readme, information });
                 }
 
                 var message = new StringBuilder()
-                    .AppendLine($"{nameof(BuildVersion)}: 成功更新文件“{file}”")
+                    .AppendLine($"{nameof(BuildVersion)}: 成功更新文件“{informationVersionFile}”")
                     .Append($"\t{information}");
                 Console.WriteLine(message.ToString());
             }
@@ -57,6 +64,94 @@ namespace BuildVersion
             }
 
         }
+        private static void AmendProjectFile(string path)
+        {
+            var project = Directory.EnumerateFiles(path, "*.csproj").SingleOrDefault();
+            if (project == null)
+            {
+                throw new Exception();
+            }
+            var xml = XElement.Load(project);
+            var sdk = xml.Attribute("Sdk");
+            var isNewProjectFormat = sdk != null;//新版本
+
+            const string propertyGroupName = "PropertyGroup";
+            const string attributeElementName = "GenerateAssemblyInformationalVersionAttribute";
+            const string compileInclude = "Properties\\InformationVersion.cs";
+
+            XNamespace ns = XNamespace.None;
+            if (!isNewProjectFormat)
+            {
+                ns = "http://schemas.microsoft.com/developer/msbuild/2003";
+            }
+
+            var propertyGroup = xml.Elements(ns + propertyGroupName)
+                .FirstOrDefault(it => it.Attribute("Condition") == null);
+            if (propertyGroup == null)
+            {
+                propertyGroup = new XElement(ns + propertyGroupName);
+                xml.Add(propertyGroup);
+                propertyGroup.Add(new XElement(ns + attributeElementName, false));
+            }
+            else
+            {
+                var element = propertyGroup.Element(ns + attributeElementName);
+                if (element != null)
+                {
+                    element.SetValue(false);
+                }
+                else
+                {
+                    propertyGroup.Add(new XElement(ns + attributeElementName, false));
+                }
+            }
+
+            if (!isNewProjectFormat)
+            {
+                var informationCompile = xml.Descendants(ns + "Compile")
+                    .FirstOrDefault(it => (string)it.Attribute("Include") == compileInclude);
+                if (informationCompile == null)
+                {
+                    var itemGroup = xml.Elements(ns + "ItemGroup").FirstOrDefault(it => it.Attribute("Condition") == null);
+                    if (itemGroup == null)
+                    {
+                        itemGroup = propertyGroup;
+                    }
+                    itemGroup.AddAfterSelf(
+                        new XElement(ns + "ItemGroup",
+                            new XElement(ns + "Compile",
+                                new XAttribute("Include", compileInclude))));
+                }
+            }
+
+
+            xml.Save(project);
+        }
+
+        private static void AmendPropertyInfo(string propertiesDir)
+        {
+            var assemblyInfoFile = Path.Combine(propertiesDir, "AssemblyInfo.cs");
+            if (File.Exists(assemblyInfoFile))
+            {
+                var needRewrite = false;
+                var assemblyInfo = new StringBuilder();
+                foreach (var item in File.ReadAllLines(assemblyInfoFile))
+                {
+                    var line = item;
+                    if (line.Contains("AssemblyInformationalVersion"))
+                    {
+                        line = "//" + line;
+                        needRewrite = true;
+                    }
+                    assemblyInfo.AppendLine(line);
+                }
+                if (needRewrite)
+                {
+                    File.WriteAllText(assemblyInfoFile, assemblyInfo.ToString());
+                }
+            }
+        }
+
 
         private static string Exec(string command, string arguments, string directory)
         {
@@ -73,7 +168,6 @@ namespace BuildVersion
             proc.WaitForExit();
             return proc.StandardOutput.ReadToEnd().Trim();
         }
-
 
         private static string BuildInformation(string path)
         {
@@ -102,7 +196,7 @@ namespace BuildVersion
             throw new Exception($"未找到对应的git目录");
         }
 
-        private static void AmendRepository(string projectPath, string gitPath, string uniqueName)
+        private static void AmendRepository(string projectDir, string gitDir, string uniqueName)
         {
             using (var mutex = new Mutex(true, uniqueName, out var created))
             {
@@ -111,13 +205,13 @@ namespace BuildVersion
 
                 try
                 {
-                    var file = Path.Combine(gitPath, nameof(BuildVersion));
+                    var file = Path.Combine(gitDir, nameof(BuildVersion));
                     if (File.Exists(file)) { return; }
 
                     File.WriteAllText(file, "请勿删除此文件");
 
                     // 在.gitignore文件中添加“InformationVersion.cs”
-                    var ignore = Path.Combine(projectPath, ".gitignore");
+                    var ignore = Path.Combine(projectDir, ".gitignore");
                     using (var stream = File.Open(ignore, FileMode.OpenOrCreate, FileAccess.ReadWrite))
                     {
                         var reader = new StreamReader(stream, Encoding.UTF8);
