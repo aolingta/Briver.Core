171079291c9eb4134000c9847bf94b73a99933a9
陈勇江 <chenyj796@126.com>
2019-01-15 17:35:25 +0800

    修改Composition

--- Core/Briver.Core/Framework/Composition.cs
+++ Core/Briver.Core/Framework/Composition.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Composition;
 using System.Composition.Hosting;
@@ -90,6 +91,66 @@ namespace Briver.Framework
     }
 
     /// <summary>
+    /// 扩展方法
+    /// </summary>
+    public static partial class CompositionExtensions
+    {
+        private class CompositionMetadata : ICompositionMetadata
+        {
+            public static readonly CompositionMetadata Empty = new CompositionMetadata(null, 0, null, null);
+
+            public CompositionMetadata(string name, int priority, string displayName, string description)
+            {
+                this.Name = name ?? string.Empty;
+                this.Priority = priority;
+                this.DisplayName = displayName ?? string.Empty;
+                this.Description = description ?? string.Empty;
+            }
+
+            public CompositionMetadata(Type type, CompositionAttribute attribute)
+            {
+                this.Name = attribute.Name ?? type.Name;
+                this.Priority = attribute.Priority;
+                this.DisplayName = attribute.DisplayName ?? this.Name;
+                this.Description = attribute.Description ?? string.Empty;
+            }
+
+            public string Name { get; }
+            public int Priority { get; }
+            public string DisplayName { get; }
+            public string Description { get; }
+        }
+
+        private static readonly ConcurrentDictionary<Type, ICompositionMetadata> _metadatas = new ConcurrentDictionary<Type, ICompositionMetadata>();
+
+        /// <summary>
+        /// 获取组件的元数据
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
+        public static ICompositionMetadata GetCompositionMetadata(this IComposition @this)
+        {
+            if (@this == null)
+            {
+                return CompositionMetadata.Empty;
+            }
+
+            return _metadatas.GetOrAdd(@this.GetType(), type =>
+            {
+                var attribute = type.GetCustomAttribute<CompositionAttribute>(false);
+                if (attribute != null)
+                {
+                    return new CompositionMetadata(type, attribute);
+                }
+
+                return new CompositionMetadata(type.Name, 0, type.Name, string.Empty);
+            });
+
+        }
+    }
+
+
+    /// <summary>
     /// 实现对导出进行排序的功能
     /// </summary>
     internal class DelegatingCompositionContext : CompositionContext
@@ -106,9 +167,9 @@ namespace Briver.Framework
         {
             public int Compare(IComposition x, IComposition y)
             {
-                var xx = x?.GetCompositionMetadata().Priority;
-                var yy = y?.GetCompositionMetadata().Priority;
-                return (yy.GetValueOrDefault(0)).CompareTo(xx.GetValueOrDefault(0));
+                var xx = x.GetCompositionMetadata();
+                var yy = y.GetCompositionMetadata();
+                return yy.Priority.CompareTo(xx.Priority);
             }
         }
 
